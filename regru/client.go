918e50bacb03f94e7d6f67832ffab2a26a9297c8
Stepan <cyberbob61@gmail.com>
2025-01-26 11:33:03 +0300

    playing

--- regru/client.go
+++ regru/client.go
@@ -1,6 +1,7 @@
 package regru
 
 import (
+	"bytes"
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
@@ -63,6 +64,49 @@ func NewClient(username, password, apiEndpoint, certFile, keyFile string) (*Clie
 	return client, nil
 }
 
+//func (c Client) doRequest(request any, fragments ...string) (*APIResponse, error) {
+//	endpoint := c.baseURL.JoinPath(fragments...)
+//
+//	inputData, err := json.Marshal(request)
+//	if err != nil {
+//		return nil, fmt.Errorf("failed to create input data: %w", err)
+//	}
+//
+//	query := endpoint.Query()
+//	query.Add("input_data", string(inputData))
+//	query.Add("input_format", "json")
+//	endpoint.RawQuery = query.Encode()
+//
+//	req, err := http.NewRequest(http.MethodGet, endpoint.String(), http.NoBody)
+//	if err != nil {
+//		return nil, fmt.Errorf("unable to create request: %w", err)
+//	}
+//
+//	resp, err := c.HTTPClient.Do(req)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	defer func() { _ = resp.Body.Close() }()
+//
+//	if resp.StatusCode/100 != 2 {
+//		return nil, parseError(req, resp)
+//	}
+//
+//	raw, err := io.ReadAll(resp.Body)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	var apiResp APIResponse
+//	err = json.Unmarshal(raw, &apiResp)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	return &apiResp, nil
+//}
+
 func (c Client) doRequest(request any, fragments ...string) (*APIResponse, error) {
 	endpoint := c.baseURL.JoinPath(fragments...)
 
@@ -71,17 +115,14 @@ func (c Client) doRequest(request any, fragments ...string) (*APIResponse, error
 		return nil, fmt.Errorf("failed to create input data: %w", err)
 	}
 
-	query := endpoint.Query()
-	query.Add("input_data", string(inputData))
-	query.Add("input_format", "json")
-	endpoint.RawQuery = query.Encode()
-
-	req, err := http.NewRequest(http.MethodGet, endpoint.String(), http.NoBody)
+	req, err := http.NewRequest(http.MethodPost, endpoint.String(), bytes.NewReader(inputData))
 	if err != nil {
 		return nil, fmt.Errorf("unable to create request: %w", err)
 	}
 
-	resp, err := c.HTTPClient.Do(req)
+	req.Header.Set("Content-Type", "application/json")
+	httpClient := c.HTTPClient
+	resp, err := httpClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
