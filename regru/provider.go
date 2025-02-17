b7278b57959fc388b75dd0bbc0e1191bbe46d316
Stepan <cyberbob61@gmail.com>
2025-02-17 22:48:13 +0300

    playing

--- regru/provider.go
+++ regru/provider.go
@@ -7,6 +7,50 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
+//const defaultApiEndpoint = "https://api.reg.ru/api/regru2/zone/add_alias"
+
+//func Provider() *schema.Provider {
+//	return &schema.Provider{
+//		Schema: map[string]*schema.Schema{
+//			"api_username": {
+//				Type:        schema.TypeString,
+//				Required:    true,
+//				DefaultFunc: schema.EnvDefaultFunc("REGRU_API_USERNAME", nil),
+//				Description: "API username for reg.ru",
+//			},
+//			"api_password": {
+//				Type:        schema.TypeString,
+//				Required:    true,
+//				Sensitive:   true,
+//				DefaultFunc: schema.EnvDefaultFunc("REGRU_API_PASSWORD", nil),
+//				Description: "API password for reg.ru",
+//			},
+//			"api_endpoint": {
+//				Type:        schema.TypeString,
+//				Optional:    true,
+//				Default:     defaultApiEndpoint,
+//				Description: "reg.ru API endpoint",
+//			},
+//			"cert_file": {
+//				Type:        schema.TypeString,
+//				Optional:    true,
+//				DefaultFunc: schema.EnvDefaultFunc("REGRU_CERT_FILE", nil),
+//				Description: "Path to the client SSL certificate file",
+//			},
+//			"key_file": {
+//				Type:        schema.TypeString,
+//				Optional:    true,
+//				DefaultFunc: schema.EnvDefaultFunc("REGRU_KEY_FILE", nil),
+//				Description: "Path to the client SSL key file",
+//			},
+//		},
+//		ResourcesMap: map[string]*schema.Resource{
+//			"regru_dns_record": resourceRegruDNSRecord(),
+//		},
+//		ConfigureFunc: configureProvider,
+//	}
+//}
+
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
